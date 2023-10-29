3c5706e495cc46ff2e77578e2f68afd2950aad69
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-10-30 02:12:05 +0530

    Update Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,11 +7,11 @@ function Hero() {
     return (
         <div className="Hero">
             <div className="hero-content center">
-                <h2 className="heading">Exclusive Print and Artwork</h2>
+                <h2 className="heading">Your Shopping Destination</h2>
                 <p className="subheading">
-                    Exclusive Art Pieces, for the Exclusive You.
+                    Simplify your shopping experience with us
                 </p>
-                <button onClick={() => navigate('/category')} className="cta btn-primary">Explore more</button>
+                <button onClick={() => navigate('/category')} className="cta btn-primary">Explore</button>
             </div>
         </div>
     );
